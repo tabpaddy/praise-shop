e659e29d079985b9db2d25461a4add90e4eadc19
praisethedeveloper <taborotap@gmail.com>
2025-01-13 00:14:30 +0100

    added context api to manage user data

---
+++ src/components/context/UserContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const updateUser = (userData) => {
+    setUser(userData);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, updateUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
