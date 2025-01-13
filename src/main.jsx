59fdb2fce432550bb37c094ec546ce81b5a40153
praisethedeveloper <taborotap@gmail.com>
2025-01-13 22:31:22 +0100

    resolve the conflict of the userprovider and provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { UserProvider } from "./components/contexts/UserContext"; // Adjust the path as needed
+import { UserProvider } from "./components/context/UserContext"; // Adjust the path as needed
 import { store } from "./components/redux/Store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserProvider>
-      <Provider store={store}>
+    <Provider store={store}>
+      <UserProvider>
         <App />
-      </Provider>
-    </UserProvider>
+      </UserProvider>
+    </Provider>
   </React.StrictMode>
 );
