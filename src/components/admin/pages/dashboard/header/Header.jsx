1ad9d419d7bb7d8f3eb700519005bb22557fdff0
praisethedeveloper <taborotap@gmail.com>
2025-01-22 21:25:55 +0100

    make the header responsive for all device and also added the components to other pages component

--- src/components/admin/pages/dashboard/header/Header.jsx
+++ src/components/admin/pages/dashboard/header/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-      <header className="bg-black text-white p-4 sticky top-0 z-50">
+      <header className="bg-black text-white p-4 sticky top-0 z-40 hover:text-slate-300">
         <h1 className="text-lg font-bold">Admin Dashboard</h1>
       </header>
     );
