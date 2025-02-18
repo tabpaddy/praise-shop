a62e25acb22dde2ac6a27334307933e7358b81ab
praisethedeveloper <taborotap@gmail.com>
2025-02-18 19:27:35 +0100

    exchnage svg for an icon and added product to clientapp jsx

--- src/components/client/ClientApp.jsx
+++ src/components/client/ClientApp.jsx
@@ -2,17 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./pages/header/Header";
 import Home from "./pages/home/Home";
 import Collection from "./pages/collection/Collection";
-import Product from "./pages/productPage/product";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
 import Footer from "./pages/footer/Footer";
 import SignUp from "./pages/auth/SignUp";
 import LogIn from "./pages/auth/LogIn";
 import PasswordReset from "./pages/auth/PasswordReset";
+import Product from "./pages/productPage/Product";
 
 export default function ClientApp() {
   return (
-    <div className="xl:mx-20 lg:mx-16 md:mx-8 sm:mx-4 mx-2">
+    <div className="2xl:mx-52 3xl:mx-80 xl:mx-28 lg:mx-16 md:mx-8 sm:mx-4 mx-2">
       <>
         <Header />
         <Routes>
