5f6fbadfc2cc78b3831b6f7f29c71c6f9ed67e34
praisethedeveloper <taborotap@gmail.com>
2025-03-10 21:15:27 +0100

    dependency update and using env in project

--- src/components/axiosInstance/api.js
+++ src/components/axiosInstance/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_REACT_APP_API_BASE_URL,
   withCredentials: true,
   xsrfCookieName: 'XSRF-TOKEN',    // Add this
   xsrfHeaderName: 'X-XSRF-TOKEN',  // Add this
